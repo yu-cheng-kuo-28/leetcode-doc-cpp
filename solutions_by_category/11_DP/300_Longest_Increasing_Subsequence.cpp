210e6afb8418fd06d5d560f2c4aa4f8458c53e8b
yu-cheng-kuo-28 <yc.kuo.28@gmail.com>
2026-02-08 07:09:39 +0800

    Update DP

--- solutions_by_category/11_DP/300_Longest_Increasing_Subsequence.cpp
+++ solutions_by_category/11_DP/300_Longest_Increasing_Subsequence.cpp
@@ -7,16 +7,38 @@ https://leetcode.com/problems/longest-increasing-subsequence/description/
 #include <algorithm>
 using namespace std;
 
+// O(n log n) | Note the cool logic here to find LIS!
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> arr;
+
+        for (int num : nums) {
+            // For a sorted vector, find the position of the smallest element >= num
+            auto it = lower_bound(arr.begin(), arr.end(), num);
+            if (it == arr.end()) {
+                arr.push_back(num);
+            } else {
+                *it = num;
+            }
+        }
+
+        return arr.size();
+    }
+};
+
+
+/* O(n^2)
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<int> f(n, 1);
-        
+
         for (int i = 1; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
                 if (nums[j] < nums[i]) {
-                    f[i] = max(f[i], f[j] + 1);
+                    f[i] = max(f[i], f[j] + 1); // Note the state transition equation
                 }
             }
         }
@@ -24,6 +46,7 @@ public:
         return *max_element(f.begin(), f.end());
     }
 };
+*/
 
 /*
 Though process:
---
+++ solutions_by_category/11_DP/3825_Longest Strictly_Increasing_Subsequence_With_Non-Zero_Bitwise_AND.cpp
@@ -0,0 +1,46 @@
+/*
+3825. Longest Strictly Increasing Subsequence With Non-Zero Bitwise AND
+https://leetcode.com/problems/longest-strictly-increasing-subsequence-with-non-zero-bitwise-and/description/
+*/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        int cntMax = 0;
+
+        for (int b = 0; b < 31; ++b) {
+            vector<int> filtered;
+            int mask = 1 << b;
+
+            for (int num : nums) {
+                if (num & mask) {
+                    filtered.push_back(num);
+                }
+            }
+
+            if (!filtered.empty()) {
+                cntMax = max(cntMax, LIS(filtered));
+            }
+        }
+
+        return cntMax;
+    }
+
+private:
+    int LIS(const vector<int>& nums) {
+        // O(n log n)
+        vector<int> dp;
+        for (int num : nums) {
+            auto it = lower_bound(dp.begin(), dp.end(), num);
+            if (it == dp.end()) {
+                dp.push_back(num);
+            } else {
+                *it = num;
+            }
+        }
+        return dp.size();
+    }
+};
